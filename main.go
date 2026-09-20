c2fe66df66fee29491d70c33ba10c2c4b43ea116
Alexis BIANCOTTO <alexis.biancotto@ynov.com>
2026-09-20 15:24:15 +0200

    main

--- main.go
+++ main.go
@@ -1,11 +1,35 @@
 package main
 
-import projetred "projetred/src"
+import (
+	"fmt"
+
+	projetred "projetred/src"
+)
 
 func main() {
 	c := projetred.InitCharacter("Alexis", "Elfe", 1, 100, 40, map[string]int{"Potion de vie": 3})
-	c.DisplayInfo()
-	c.AccessInventory()
-	c.TakePot()
-	c.DisplayInfo()
+
+	for {
+		fmt.Println()
+		fmt.Println("=== MENU ===")
+		fmt.Println("1. Afficher les informations du personnage")
+		fmt.Println("2. Accéder à l'inventaire")
+		fmt.Println("3. Quitter")
+		fmt.Print("Ton choix : ")
+
+		var choice int
+		fmt.Scanln(&choice)
+
+		switch choice {
+		case 1:
+			c.DisplayInfo()
+		case 2:
+			c.AccessInventory()
+		case 3:
+			fmt.Println("À bientôt !")
+			return
+		default:
+			fmt.Println("Choix invalide, réessaie.")
+		}
+	}
 }

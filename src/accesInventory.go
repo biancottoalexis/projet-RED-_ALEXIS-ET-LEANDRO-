b4c425aabaf33419aa2c43b45a0a55174bf9abca
Alexis BIANCOTTO <alexis.biancotto@ynov.com>
2026-09-21 09:30:50 +0200

    accesInventory

--- src/accesInventory.go
+++ src/accesInventory.go
@@ -11,10 +11,42 @@ func (c *Character) AccessInventory() {
 	fmt.Println("=== INVENTAIRE ===")
 
 	i := 1
+	var items []string
 	for item, quantity := range c.Inventory {
+		if quantity <= 0 {
+			continue
+		}
 		fmt.Printf("%d. %s x%d\n", i, item, quantity)
+		items = append(items, item)
 		i++
 	}
-
 	fmt.Println("0. Fermer l'inventaire")
+	fmt.Print("Ton choix : ")
+
+	var choice int
+	fmt.Scanln(&choice)
+
+	if choice <= 0 || choice > len(items) {
+		return
+	}
+
+	selected := items[choice-1]
+	c.UseItem(selected)
+}
+
+func (c *Character) UseItem(name string) {
+	switch name {
+	case "Potion de vie":
+		c.TakePot()
+	case "Potion de poison":
+		c.PoisonPot()
+	case "Chapeau de l'aventurier":
+		c.EquipHead(name)
+	case "Tunique de l'aventurier":
+		c.EquipChest(name)
+	case "Bottes de l'aventurier":
+		c.EquipFeet(name)
+	default:
+		fmt.Println("Cet objet ne peut pas être utilisé")
+	}
 }
